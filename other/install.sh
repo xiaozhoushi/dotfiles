3a86c3f4874724b38bd685130384623da2b3425b
xiaozhoushi <shixiaozhou0913@gmail.com>
2023-05-29 23:26:56 +0800

    update ubuntu install

--- other/install.sh
+++ other/install.sh
@@ -3,13 +3,16 @@
 set -o nounset # error when referencing undefined variable
 set -o errexit # exit when command fails
 
+
 install() {
 	[ "$(uname)" == "Darwin" ] && installMac
 	[ -n "$(uname -a | grep Ubuntu)" ] && installUbuntu
+	[ -n "$(uname -a | grep WSL2)" ] && installWsl2
 	[ -n "$(uname -a | grep Debian)" ] && installDebian
 	[ -f "/etc/arch-release" ] && installArch
 	[ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ] && echo "Windows not currently supported"
 }
+
 
 installMac() {
 	echo "update source..."
@@ -25,27 +28,27 @@ installMac() {
 	brew install lua
 	brew install node
 	brew install yarn
-	
-
 
 
 	brew install ripgrep fzf ranger
 	npm install -g tree-sitter-cli
 }
 
+
 installUbuntu() {
 	echo "update source..."
 	sudo apt update
-	which pip3 >/dev/null && echo "pip installed, moving on..." || sudo apt install python3-pip >/dev/null
-	which node >/dev/null && echo "node installed, moving on..." || sudo apt install nodejs
-	sudo apt install npm
+  sudo apt upgrade
+  sudo apt install -y libreadline-dev libffi-dev libsqlite3-dev libbz2-dev liblzma-dev git curl \
+    wget build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev net-tools
+	which pip3 >/dev/null && echo "pip installed, moving on..." || sudo apt install -y python3-pip >/dev/null
+	which node >/dev/null && echo "node installed, moving on..." || sudo apt install -y nodejs
+	sudo apt install -y npm
 	mkdir ~/.npm-global
 	npm config set prefix '~/.npm-global'
 
-	sudo apt install tmux
-	apt install ranger
-  mv lvim lvim.new
-  mv lvim.old lvim
+	sudo apt install -y tmux
+	sudo apt install -y ranger
 
 	echo "install zsh..."
 	sudo apt install -y zsh
@@ -55,13 +58,8 @@ installUbuntu() {
 
 	echo "install ohmyzsh..."
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-	ln -s -f ../.zshrc ~/.zshrc
+	ln -s -f ~/.config/.zshrc ~/.zshrc
 
-	'''
-	echo "install anaconda..."
-	wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
-	sh ...
-	'''
 
 	echo "install neovim..."
 	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
@@ -75,22 +73,19 @@ installUbuntu() {
 	npm install -g tree-sitter-cli
 	pip3 install pynvim -i http://pypi.douban.com/simple/ --trusted-host pypi.douban.com
 	curl --proto '=https' --tlsv1.3 -sSf https://sh.rustup.rs | sh
-	#rustup toolchain install nightly #指定新版本
 	rustup default nightly
 
 	echo "install lunarvim..."
-	#bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) #修改对应nvim版本
-	LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
-
+  LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+  #mv ~/.config/lvim ~/.config/lvim.new
+  #mv ~/.config/lvim.old ~/.config/lvim
 
 	echo "install tmux..."
-	git clone https://github.com/gpakosz/.tmux.git
-	cp .tmux/.tmux.conf ../.tmux.conf
-	ln -s -f ../.tmux.conf ~/.tmux.conf
-	cp .tmux/.tmux.conf.local ~/.tmux.conf.local
-	rm -rf .tmux
-
-
+	git clone https://github.com/gpakosz/.tmux.git ~/.config/.tmux
+	cp ~/.config/.tmux/.tmux.conf ~/.config/.tmux.conf
+	ln -s -f ~/.config/.tmux.conf ~/.tmux.conf
+	cp ~/.config/.tmux/.tmux.conf.local ~/.tmux.conf.local
+	rm -rf ~/.config/.tmux
 
 	echo "install lazygit..."
 	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
@@ -99,18 +94,13 @@ installUbuntu() {
 	sudo install lazygit /usr/local/bin
   rm lazygit*
 
-
 	echo "install other..."
 	#apt install w3m-img
-	apt install hyperfine
-	apt install bat fd-find
-	sudo apt install ripgrep fzf
-	sudo apt install libjpeg8-dev zlib1g-dev libxtst-dev
-  sudo apt install python-dev python3-dev
-  sudo apt install net-tools
+	sudo apt install hyperfine
+	sudo apt install libjpeg8-dev
+  sudo apt install -y bat fd-find ripgrep fzf libjpeg8-dev zlib1g-dev libxtst-dev python3-dev  thefuck
 	#pip3 install ueberzug
 	pip3 install neovim-remote
-	sudo apt install -y thefuck
 
 	echo "install docker..."
 	curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
@@ -118,8 +108,13 @@ installUbuntu() {
 	echo "install lazydocker..."
 	curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 
-
+	'''
+	echo "install anaconda..."
+	wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
+	sh ...
+	'''
 }
+
 
 installDebian() {
 	echo "update source..."
@@ -163,7 +158,6 @@ installDebian() {
 	echo "install docker..."
 	curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
 
-
 	echo "install lazygit..."
 	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
@@ -187,11 +181,8 @@ installDebian() {
 	pip3 install neovim-remote
 	sudo apt install -y thefuck
 
-
-
-
-
 }
+
 
 installArch() {
 	echo "update source..."
